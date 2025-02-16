caf25b7c7e107b1f2fd1c2bbe360b453f7e10d8c
Tom Esposito <tomesposito4@gmail.com>
2025-02-16 12:00:37 -0600

    updates for deploy errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,12 @@ set -e
 
 # Get the AWS Account ID and Region
 ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
-REGION=$(aws configure get region)
+REGION="us-east-1"
 
 echo "Bootstrapping CDK in account: $ACCOUNT, region: $REGION"
+
+npm install aws-cdk-lib constructs
+npm install --save-dev @types/node
 
 # Bootstrap the CDK environment dynamically
 npx cdk bootstrap aws://$ACCOUNT/$REGION
