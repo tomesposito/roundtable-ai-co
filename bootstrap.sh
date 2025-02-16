e1fec46af86a3859a6919e13b46d65b26d579963
Tom Esposito <tomesposito4@gmail.com>
2025-02-16 12:08:07 -0600

    tweaking cypress tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Clear Cypress cache to free up space
+rm -rf ~/.cache/Cypress
+
 # Get the AWS Account ID and Region
 ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 REGION="us-east-1"
@@ -9,6 +12,10 @@ echo "Bootstrapping CDK in account: $ACCOUNT, region: $REGION"
 
 npm install aws-cdk-lib constructs
 npm install --save-dev @types/node
+
+# Optionally, skip Cypress installation if not needed in this environment.
+# For example, you might set an environment variable to skip installing dev dependencies.
+export CI_SKIP_CYPRESS=true
 
 # Bootstrap the CDK environment dynamically
 npx cdk bootstrap aws://$ACCOUNT/$REGION
